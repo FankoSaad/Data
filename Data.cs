61f39edae6a2d55116474897c4f39c1140b88579
Engy Saad <161323611+FankoSaad@users.noreply.github.com>
2024-03-22 18:54:11 +0200

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace test;
+[JsonArray]
+public class Locations
+{
+    public string? Name { get; set; }
+    public string? Street { get; set; }
+    public string? Region { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+    public string? Hotline { get; set; }
+    public string? Type { get; set; }
+    public string? Location { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Json = File.ReadAllText(@"/Users/engysaad/Projects/Locations/Locations/Json.json");
+        Locations? ElEzabyPharmacies = JsonConvert.DeserializeObject<Locations>(Json);
+        Console.WriteLine(ElEzabyPharmacies?.Country);
+        Console.ReadLine();
+    }
+}
